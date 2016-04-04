c05261a382923a71405de4dfcd8ddb018a872dff
appwebtech <josembi@gmail.com>
2016-04-04 23:18:16 +0200

    new methods, posts generate, new files and variables

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @content_first = 'This is  a paragraph which is very cool innit?';
+    @content_second = 'This paragrpah wish show immediately after the horizontal line that my gf is asking about.';
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def show
+    
+  end
+
+  def destroy
+    
+  end
+end
